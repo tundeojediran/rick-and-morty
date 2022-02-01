ce674d026b8515f06fa355c5d1d8d1c7dca81c15
Daniel Tunde Ojediran <tundeojediran@gmail.com>
2022-02-01 09:20:57 +0100

    update responsiveness

--- src/RickAndMortyApp.tsx
+++ src/RickAndMortyApp.tsx
@@ -1,41 +1,36 @@
-import { Grid } from '@mui/material';
-import React, { Component } from 'react';
-import './App.css';
-import OutlinedCard from './components/OutlinedCard';
-import styled from '@emotion/styled';
-import Character from './components/Character';
-import RickMortyService from './services/RickMortyService';
-
+import { Grid } from "@mui/material";
+import React, { Component } from "react";
+import "./App.css";
+import OutlinedCard from "./components/OutlinedCard";
+import styled from "@emotion/styled";
+import Character from "./components/Character";
+import RickMortyService from "./services/RickMortyService";
 
 const Wrapper = styled.div`
-    @media (min-width: 700px) {
-        display: flex;
-        top: 64px;
-        position: relative;
-        height: calc(100% - 64px);
-        flex-direction: column;
-        padding: 24px;
-        background-color: #EDEDED;
-    }
+  @media (min-width: 700px) {
+    display: flex;
+    top: 64px;
+    position: relative;
+    height: calc(100% - 64px);
+    flex-direction: column;
+    padding: 24px;
+    background-color: #ededed;
+  }
 `;
 
+interface AppProps {}
 
-interface AppProps {
-  // message: string;
-};
 interface AppState {
   characters: any[];
-};
+}
 
 class RickAndMortyApp extends Component<AppProps, AppState> {
-
   constructor(props: AppProps) {
     super(props);
 
     this.state = {
-      characters: [],
-
-    }
+      characters: []
+    };
   }
 
   componentDidMount() {
@@ -44,8 +39,8 @@ class RickAndMortyApp extends Component<AppProps, AppState> {
 
   fetchData = async () => {
     const data = await RickMortyService.fetchCharacters();
-    this.setState({ characters: data.results })
-  }
+    this.setState({ characters: data.results });
+  };
 
   render() {
     const { characters } = this.state;
@@ -54,21 +49,19 @@ class RickAndMortyApp extends Component<AppProps, AppState> {
       <>
         <Wrapper>
           <Grid container spacing={2}>
-            {characters.length > 0 && characters.map(character => {
-              return (
-                <Grid key={character.id} item xs={1} sm={2} md={3}>
-                  <Character data={character} />
-                </Grid>
-              )
-            })}
+            {characters.length > 0 &&
+              characters.map((character) => {
+                return (
+                  <Grid key={character.id} item xs={12} md={6}>
+                    <Character data={character} />
+                  </Grid>
+                );
+              })}
           </Grid>
         </Wrapper>
-
       </>
-
     );
   }
-
 }
 
 export default RickAndMortyApp;
